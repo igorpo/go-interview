68545e9c62eaf930a07cd25a65494c27893aec40
Igor Pogorelskiy <ipogorelskiy@gmail.com>
2018-07-30 22:24:37 -0700

    Rotate done

--- arrays/rotate_matrix.go
+++ arrays/rotate_matrix.go
@@ -2,11 +2,18 @@ package arrays
 
 // Given an NxN matrix, rotate it by 90 degress clockwise in place
 
-// We do not actually need to return it since it is in place but for the sake of
-// the testing for the exercise we can.
+// Key here is to understand that M[i][j] -> M[n-j-1][i] in rotated matrix
 func RotateMatrix90CW(matrix [][]int) {
 	n := len(matrix[0])
 
+	// cycle through the layers
 	for i := 0; i < n/2; i++ {
+		for j := i; j < n-i-1; j++ {
+			temp := matrix[i][j]
+			matrix[i][j] = matrix[n-j-1][i]
+			matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+			matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+			matrix[j][n-i-1] = temp
+		}
 	}
 }
